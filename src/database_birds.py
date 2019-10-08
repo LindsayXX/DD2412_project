273478c022226f29e8a2ee9a09b2a86400ca4789
Romina Arriaza <rarriaza@alges.cl>
2019-10-08 19:10:54 +0200

    adding database_birds class

---
+++ src/database_birds.py
@@ -0,0 +1,24 @@
+import IPython.display as display
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import os
+
+class bird:
+
+    def __init__(self):
+        label = None
+        image = None
+        parts = None
+
+class database:
+
+    def __init__(self):
+        BATCH_SIZE = 32
+        IMG_HEIGHT = 224
+        IMG_WIDTH = 224
+        #STEPS_PER_EPOCH = np.ceil(image_count / BATCH_SIZE)
+
+    def load_labels(self, label_path):
+        pass
