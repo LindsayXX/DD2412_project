4eb44bede80d5618bc00be4123cd2657e8f9efc2
xlxxx <lingxi.xiong@foxmail.com>
2019-10-22 19:20:09 +0200

    region crop net

--- cropnet.py
+++ cropnet.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 class RCN(tf.keras.Model):
-    def __init__(self, hidden_unit, map_size=14):
+    def __init__(self, hidden_unit, map_size=14, image_size=448):
         super(RCN, self).__init__()
         self.flat = tf.keras.layers.Flatten()
         self.fc1 = tf.keras.layers.Dense(hidden_unit, activation="relu")#, input_shape=(map_size * map_size))
@@ -17,13 +17,19 @@ class RCN(tf.keras.Model):
         out = self.fc1(out)
         out = self.fc2(out)
         out = self.activation(out)
-        out = tf.dtypes.cast(out, tf.int32)
+        #out = tf.dtypes.cast(out, tf.int32)
         mask = self.boxcar(out)
 
         return mask
 
     def boxcar(self, t, k=10):
-        V = tf.zeros([t.shape[0], self.map_size, self.map_size])
+        #V = tf.zeros([t.shape[0], self.map_size, self.map_size])
+        V = []
+        X = tf.range(start=0, limit=self.map_size)
+        Y = tf.range(start=0, limit=self.map_size)
+        X, Y = tf.meshgrid(X, Y)
+        X = tf.dtypes.cast(X, tf.float32)
+        Y = tf.dtypes.cast(Y, tf.float32)
         for n in range(t.shape[0]):
             '''
             for i in range(V.shape[1]):
@@ -32,14 +38,12 @@ class RCN(tf.keras.Model):
                     V_y = 1 / (1 + tf.math.exp(-k * (j - t[n, 1] + 0.5 * t[n, 2]))) - 1 / (1 + tf.math.exp(-k * (j - t[n, 1] - 0.5 * t[n, 2])))
                     V[n, i, j] = tf.multiply(V_x, V_y)
             '''
-            X = tf.range(start=0, limit=V.shape[1])
-            Y = tf.range(start=0, limit=V.shape[2])
-            X, Y = tf.meshgrid(X, Y)
-            V_x = tf.constant(1) / (tf.constant(1) + tf.math.exp(-k * (X - t[n, 0] + t[n, 2]/tf.constant(2)))) - tf.constant(1) / (tf.constant(1) + tf.math.exp(-k * (X - t[n, 0] - t[n, 2]/tf.constant(2))))
-            V_y = tf.constant(1) / (tf.constant(1) + tf.math.exp(-k * (Y - t[n, 1] + t[n, 2]/tf.constant(2)))) - tf.constant(1) / (tf.constant(1) + tf.math.exp(-k * (Y - t[n, 1] - t[n, 2]/tf.constant(2))))
-            V[n] = tf.multiply(V_x, V_y)
+            V_x = 1 / (tf.math.exp(-k * (tf.math.add((X-t[n,0]), t[n,2]/2))) + 1) - 1 / (1 + tf.math.exp(-k * (tf.math.subtract((X-t[n,0]), t[n,2]/2))))
+            V_y = 1 / (1 + tf.math.exp(-k * tf.math.add(Y-t[n,1], t[n,2]/2))) - 1 / (1 + tf.math.exp(-k * tf.math.subtract(Y-t[n,1], t[n,2]/2)))
+            #V[n] = tf.multiply(V_x, V_y)
+            V.append(tf.multiply(V_x, V_y))
 
-        return V
+        return tf.stack(V)
 
 #@tf.function
 def loss_CPT(map, gtmap, batch_size=32):
@@ -47,26 +51,30 @@ def loss_CPT(map, gtmap, batch_size=32):
     return tf.nn.l2_loss(diff)/batch_size
 
 def crop(image, mask, batch_size=32):
-    # upsample
-    for c in range(mask.shape[0]):
-        mask[c] = tf.image.resize_images(mask[c], [image.shape[1], image.shape[2]])
     crop_image = tf.zeros(image.shape)
-    for i in range(image.shape[0]):#batch
-        for j in range(image.shape[-1]):#channel
-            crop_image[i, :, :, j] = tf.math.multiply(image[i, :, :, j], mask)
+    #upsample
+    mask = tf.expand_dims(mask, -1)
+    mask = tf.broadcast_to(mask, [mask.shape[0], mask.shape[1], mask.shape[2], 3])
+    mask = tf.image.resize(mask, size=[image.shape[1], image.shape[2]])
+    tf.multiply(image, mask)
 
-    return crop_image
+    return crop_image, mask
 
 
 if __name__ == '__main__':
     # test
-    batch_size =  1
+    batch_size = 3
     sample_input = tf.random.normal([batch_size, 14, 14])
-    crop_net = RCN(hidden_unit=14, map_size=14)
+    crop_net = RCN(hidden_unit=14, map_size=14, image_size=448)
     mask = crop_net.call(sample_input)
     sample_image = tf.random.normal([batch_size, 448, 448, 3])
-    crop_image = crop(sample_image, mask)
+    crop_image, newmask = crop(sample_image, mask)
     for i in range(batch_size):
+        plt.subplot(131)
         plt.imshow(sample_image[i].numpy())
+        plt.subplot(132)
+        plt.imshow(newmask[i].numpy())
+        plt.subplot(133)
+        #plt.imshow(tf.multiply(sample_image[i], tf.ones(sample_image.shape))[0].numpy())
         plt.imshow(crop_image[i].numpy())
         plt.show()
